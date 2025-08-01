550d936339b6fbb0e14e3c7f887257c324880b35
Tronikel <contactdonatas@gmail.com>
2025-08-01 18:59:43 +0300

    fix: init + perf

--- lua/ts-autotag/rename_tag.lua
+++ lua/ts-autotag/rename_tag.lua
@@ -61,62 +61,66 @@ local function extmark_opts(opts)
 end
 
 ---@param bufnr integer
-local function update_sibling_extmarks(bufnr)
+---@param on_parse function
+local function update_sibling_extmarks(bufnr, on_parse)
 	local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
 	if not ok or not parser then
 		return
 	end
 
 	local cursor_row = vim.api.nvim_win_get_cursor(0)[1] - 1
-	parser:parse({ cursor_row, cursor_row })
+	parser:parse({ cursor_row, cursor_row }, function()
+		local opening_node, closing_node = get_opening_pair(bufnr)
+		if not opening_node or not closing_node then
+			closing_node, opening_node = get_closing_pair(bufnr)
+		end
+		if not opening_node or not closing_node then
+			on_parse()
+			return
+		end
+		vim.api.nvim_buf_clear_namespace(bufnr, NS_EXT, 0, -1)
 
-	local opening_node, closing_node = get_opening_pair(bufnr)
-	if not opening_node or not closing_node then
-		closing_node, opening_node = get_closing_pair(bufnr)
-	end
-	if not opening_node or not closing_node then
-		return
-	end
-	vim.api.nvim_buf_clear_namespace(bufnr, NS_EXT, 0, -1)
+		local opening_indices = ts.get_node_indices(opening_node)
+		local closing_indices = ts.get_node_indices(closing_node)
 
-	local opening_indices = ts.get_node_indices(opening_node)
-	local closing_indices = ts.get_node_indices(closing_node)
+		local opening_node_iden = ts.get_node_iden(opening_node)
+		if opening_node_iden then
+			opening_indices = ts.get_node_indices(opening_node_iden)
+		else
+			set_empty_indices(opening_indices, 1)
+		end
 
-	local opening_node_iden = ts.get_node_iden(opening_node)
-	if opening_node_iden then
-		opening_indices = ts.get_node_indices(opening_node_iden)
-	else
-		set_empty_indices(opening_indices, 1)
-	end
+		local closing_node_iden = ts.get_node_iden(closing_node)
+		if closing_node_iden then
+			closing_indices = ts.get_node_indices(closing_node_iden)
+		else
+			set_empty_indices(opening_indices, 2)
+		end
 
-	local closing_node_iden = ts.get_node_iden(closing_node)
-	if closing_node_iden then
-		closing_indices = ts.get_node_indices(closing_node_iden)
-	else
-		set_empty_indices(opening_indices, 2)
-	end
+		local id = vim.api.nvim_buf_set_extmark(
+			bufnr,
+			NS_EXT,
+			opening_indices.start_row,
+			opening_indices.start_col,
+			extmark_opts({
+				end_col = opening_indices.end_col,
+				end_row = opening_indices.end_row,
+			})
+		)
+		vim.api.nvim_buf_set_extmark(
+			bufnr,
+			NS_EXT,
+			closing_indices.start_row,
+			closing_indices.start_col,
+			extmark_opts({
+				id = id + 1,
+				end_col = closing_indices.end_col,
+				end_row = closing_indices.end_row,
+			})
+		)
 
-	local id = vim.api.nvim_buf_set_extmark(
-		bufnr,
-		NS_EXT,
-		opening_indices.start_row,
-		opening_indices.start_col,
-		extmark_opts({
-			end_col = opening_indices.end_col,
-			end_row = opening_indices.end_row,
-		})
-	)
-	vim.api.nvim_buf_set_extmark(
-		bufnr,
-		NS_EXT,
-		closing_indices.start_row,
-		closing_indices.start_col,
-		extmark_opts({
-			id = id + 1,
-			end_col = closing_indices.end_col,
-			end_row = closing_indices.end_row,
-		})
-	)
+		on_parse()
+	end)
 end
 
 ---@param bufnr integer
@@ -173,18 +177,53 @@ local function sync_pair(bufnr, pair_id_offset)
 	end
 
 	vim.api.nvim_buf_set_text(bufnr, ext2[1], ext2[2], ext2[3].end_row, ext2[3].end_col, { text1 })
-
-	-- local cursor = vim.api.nvim_win_get_cursor(0)
-	-- vim.api.nvim_win_set_cursor(0, { cursor[1], cursor[2] - 2 })
-	-- update_sibling_extmarks(bufnr)
-	-- vim.api.nvim_win_set_cursor(0, cursor)
 end
 
 ---@param bufnr integer
 local function sync(bufnr)
-	update_sibling_extmarks(bufnr)
-	sync_pair(bufnr, 1)
-	sync_pair(bufnr, -1)
+	local stale_cursor = vim.api.nvim_win_get_cursor(0)
+	update_sibling_extmarks(bufnr, function()
+		local cursor = vim.api.nvim_win_get_cursor(0)
+		vim.api.nvim_win_set_cursor(0, stale_cursor)
+		sync_pair(bufnr, 1)
+		sync_pair(bufnr, -1)
+		vim.api.nvim_win_set_cursor(0, cursor)
+	end)
+end
+
+---@param bufnr integer
+local function init(bufnr)
+	local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+	if not ok or not parser then
+		return
+	end
+
+	local timer = assert(vim.uv.new_timer())
+	---@param count integer
+	local function check(count)
+		if count > 50 then
+			timer:stop()
+			timer:close()
+			return
+		end
+
+		local cursor_row = vim.api.nvim_win_get_cursor(0)[1] - 1
+		if parser:is_valid(false, { cursor_row, cursor_row }) then
+			sync(bufnr)
+			timer:stop()
+			timer:close()
+			return
+		end
+
+		timer:start(
+			100,
+			0,
+			vim.schedule_wrap(function()
+				check(count + 1)
+			end)
+		)
+	end
+	check(0)
 end
 
 function M.setup()
@@ -192,30 +231,18 @@ function M.setup()
 		default = true,
 	})
 
+	if vim.api.nvim_buf_is_loaded(0) then
+		init(vim.api.nvim_get_current_buf())
+	end
+	vim.api.nvim_create_autocmd("BufRead", {
+		callback = function(ev)
+			init(ev.buf)
+		end,
+	})
+
 	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 		callback = function(ev)
-			-- local existing_ext = get_cursor_extmarks(ev.buf)
-			-- if existing_ext then
-			-- 	return
-			-- end
-
-			local t = vim.uv.hrtime() / 1000 / 1000
 			sync(ev.buf)
-			local diff = (vim.uv.hrtime() / 1000 / 1000) - t
-			if diff > 5 then
-				print(diff)
-			end
-
-			-- local group = vim.api.nvim_create_augroup("ts-autotag.nvim/CursorMoved", {})
-			--
-			-- vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-			-- 	group = group,
-			-- 	buffer = ev.buf,
-			-- 	callback = function(ev)
-			-- 		sync_pair(ev.buf, 1)
-			-- 		sync_pair(ev.buf, -1)
-			-- 	end,
-			-- })
 		end,
 	})
 end
