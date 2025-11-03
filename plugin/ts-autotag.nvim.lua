7ea7daed94640083c94e36afa862b7b12f09e651
Soifou <fleur.fr@gmail.com>
2025-11-04 00:56:52 +0100

    fix(config): allow override for `filetypes`

--- plugin/ts-autotag.nvim.lua
+++ plugin/ts-autotag.nvim.lua
@@ -2,16 +2,15 @@ local augroup = vim.api.nvim_create_augroup("ts-autotag.nvim/init_buffer", {})
 
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup,
-	pattern = require("ts-autotag.config").config.filetypes,
-	callback = function(ev)
-		local config = require("ts-autotag.config")
-
-		if config.config.auto_rename.enabled then
-			require("ts-autotag.rename_tag.auto").init(ev.buf)
-		end
-
-		if config.config.auto_close.enabled then
-			require("ts-autotag.close_tag").init(ev.buf)
+	callback = function(event)
+		local config = require("ts-autotag.config").config
+		if vim.tbl_contains(config.filetypes, event.match) then
+			if config.auto_rename.enabled then
+				require("ts-autotag.rename_tag.auto").init(event.buf)
+			end
+			if config.auto_close.enabled then
+				require("ts-autotag.close_tag").init(event.buf)
+			end
 		end
 	end,
 })
